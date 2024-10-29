af8561bf2db5ccfbd4b807b86d85d395384eb86d
Vianny Rithik Reddy P <99172274+urstrulyrithik@users.noreply.github.com>
2024-10-29 01:31:05 -0400

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
       <div className="wrapper">
         <motion.span initial={{ opacity: 0.1, scale: 0.5 }} animate={{ opacity: 1, scale: 1.3 }} transition={{ duration: 1 }} style={{marginLeft:110}}>Rithik Reddy</motion.span>
         <div className="social">
-          <a href="https://github.com/urstrulyrithik"><img src="/facebook.png" alt="" /></a>
-          <a href="https://github.com/urstrulyrithik"><img src="/instagram.png" alt="" /></a>
-          <a href="https://github.com/urstrulyrithik"><img src="/youtube.png" alt="" /></a>
-          <a href="https://github.com/urstrulyrithik"><img src="/dribbble.png" alt="" /></a>
+          <a href="https://www.linkedin.com/in/rithikreddypv/"><img src="/facebook.png" alt="" /></a>
+          <a href="https://www.linkedin.com/in/rithikreddypv/"><img src="/instagram.png" alt="" /></a>
+          <a href="https://www.linkedin.com/in/rithikreddypv/"><img src="/youtube.png" alt="" /></a>
+          <a href="https://www.linkedin.com/in/rithikreddypv/"><img src="/dribbble.png" alt="" /></a>
         </div>
       </div>
     </div>
