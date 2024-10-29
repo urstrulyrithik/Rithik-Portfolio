2cf1ac4b49c3b0eeb5631f2834f0c8f63bd93da7
Vianny Rithik Reddy P <99172274+urstrulyrithik@users.noreply.github.com>
2024-10-29 01:14:07 -0400

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -20,27 +20,48 @@ const items = [
   },
   {
     id: 3,
+    title: "Expense Tracking System",
+    projectLink:"https://github.com/urstrulyrithik/Project_Expense_Tracking_System",
+    img: "./ExpenseTracking.png",
+    description: "An Expense Management System featuring a Streamlit-based frontend and a FastAPI backend, designed for efficient tracking and management of personal expenses. The project includes a modular structure with dedicated components for frontend, backend, and comprehensive testing."
+  },
+  {
+    id: 4,
+    title: "Predictive Modeling",
+    projectLink:"https://github.com/urstrulyrithik/Predictive-Modeling",
+    img: "./PredictiveAnalytics.jpg",
+    description: "This project develops a predictive model in R to analyze and forecast based on historical data, leveraging machine learning techniques and statistical analysis. Key features include data preprocessing, exploratory data analysis, model training and selection, performance evaluation, and result visualization."
+  },
+  {
+    id: 5,
+    title: "Text Analytics",
+    projectLink:"https://github.com/urstrulyrithik/Text-Analytics",
+    img: "./TextAnalytics.jpeg",
+    description: "This project involved extensive text mining and social network analysis in R, focusing on the book 'Tarzan of the Apes' to identify significant words and sentences, perform sentiment analysis, and uncover latent topics through topic modeling. Key features included corpus preprocessing, sentiment analysis, topic modeling (LDA, CTM), and visualizations such as word clouds and sentiment trajectories."
+  },
+  {
+    id: 6,
     title: "Covid Webiste Using Full Stack Web",
     projectLink:"https://github.com/urstrulyrithik/rithik-covidWebsite",
     img: "https://images.pexels.com/photos/3992933/pexels-photo-3992933.jpeg?auto=compress&cs=tinysrgb&w=1600",
     description: "Designed and developed a Covid Website Using Full Stack Development from end to end using React JS, SQL and Express JS and CSS, whose objective is to display statistics and content regarding the pandemic and the Corona virus to users."
   },
   {
-    id: 4,
+    id: 7,
     title: "Jobby App",
     projectLink:"https://rithikjobbyapp.ccbp.tech/login",
     img: "https://images.pexels.com/photos/392018/pexels-photo-392018.jpeg?auto=compress&cs=tinysrgb&w=1600",
     description: "Designed and developed Jobby App, a front-end ReactJS website for PC users, whose objective is to enable users to discover the jobs they seek. CREDENTIALS--> USERNAME: rahul ; PASSWORD: rahul@2021"
   },
   {
-    id: 5,
+    id: 8,
     title: "Twitter Backend Clone",
     projectLink:"https://github.com/urstrulyrithik/TwitterClone",
     img: "https://images.pexels.com/photos/5417837/pexels-photo-5417837.jpeg?auto=compress&cs=tinysrgb&w=1600",
     description: "Designed Backend Twitter Clone REST APIs to recreate and carry out Twitter's backend database CRUD operations using JSON format, MySQL and Node JS (Express JS)"
   },
   {
-    id: 6,
+    id: 9,
     title: "My Portfolio Site",
     img: "./PortfolioHero.png",
     projectLink:"https://github.com/urstrulyrithik/Rithik-Portfolio",
