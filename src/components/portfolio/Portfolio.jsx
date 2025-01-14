7d8b7bbd12f0349b1207cd7f1da7f22f1b085dcc
Vianny Rithik Reddy P <99172274+urstrulyrithik@users.noreply.github.com>
2025-01-13 21:25:33 -0500

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,64 +4,71 @@ import "./portfolio.scss"
 import { motion, useScroll, useSpring, useTransform } from "framer-motion"
 
 const items = [
-    {
+  {
     id: 1,
+    title: " Healthcare Data Analyst at Apollo Hospitals",
+    projectLink:"https://github.com/urstrulyrithik",
+    img: "./CustomerAnalyst.jpg",
+    description: "As a Healthcare Data Analyst, I leveraged machine learning models built with TensorFlow and PyTorch to predict patient readmission risks with 87% accuracy, contributing to a 12% reduction in hospital readmissions. I streamlined the extraction and transformation of over 5 million patient records from MongoDB and Snowflake into a centralized analytics platform, cutting data processing time by 40%. Additionally, I conducted exploratory data analysis (EDA) on treatment adherence data using Python and Pandas, driving a 15% improvement in patient adherence rates. I also designed dynamic dashboards in Excel, improving clinical decision-making efficiency by 25%."
+  },
+    {
+    id: 2,
     title: "Data Analyst at Star Health Allied Insurance",
     projectLink:"https://github.com/urstrulyrithik",
     img: "./CustomerAnalyst.jpg",
     description: "Extracted and consolidated customer data from Snowflake, AWS RDS, and Amazon S3 using SQL and Python. Cleaned and analyzed data with Pandas and NumPy, and Excel for data validation, identifying churn risks and trends. Built logistic regression models (80% accuracy) to predict cancellations, reducing churn by 10% and saving $500K in premiums through targeted retention campaigns. Collaborated with marketing teams to design dashboards in Tableau and Power BI to communicate and implement multi-channel campaigns, driving data-driven decisions and improving customer retention rates."
   },    
   {
-    id: 2,
+    id: 3,
     title: "Operations Data Analyst at Flipkart",
     projectLink:"https://github.com/urstrulyrithik",
     img: "./OperationsAnalyst.jpg",
     description: "•	Automated ETL processes to centralize over 1M data rows in Snowflake, reducing manual data entry by 30%. Cleaned and transformed data using SAS and SQL, developing Linear Regression and ARIMA models with 85% forecast accuracy, reducing MAPE by 12%. Optimized inventory management by reducing excess stock and preventing stockouts, leading to annual savings of $96K and safeguarding $120K in revenue. Designed Tableau dashboards for 50+ stores, hosted on AWS EC2 with Amazon S3, improving decision-making efficiency by 40% and driving a 7% revenue boost through store-level insights."
   },
   {
-    id: 3,
+    id: 4,
     title: "Expense Tracking System",
     projectLink:"https://github.com/urstrulyrithik/Project_Expense_Tracking_System",
     img: "./ExpenseTracking.png",
     description: "An Expense Management System featuring a Streamlit-based frontend and a FastAPI backend, designed for efficient tracking and management of personal expenses. The project includes a modular structure with dedicated components for frontend, backend, and comprehensive testing."
   },
   {
-    id: 4,
+    id: 5,
     title: "Predictive Modeling",
     projectLink:"https://github.com/urstrulyrithik/Predictive-Modeling",
     img: "./PredictiveAnalytics.jpg",
     description: "This project develops a predictive model in R to analyze and forecast based on historical data, leveraging machine learning techniques and statistical analysis. Key features include data preprocessing, exploratory data analysis, model training and selection, performance evaluation, and result visualization."
   },
   {
-    id: 5,
+    id: 6,
     title: "Text Analytics",
     projectLink:"https://github.com/urstrulyrithik/Text-Analytics",
     img: "./TextAnalytics.jpeg",
     description: "This project involved extensive text mining and social network analysis in R, focusing on the book 'Tarzan of the Apes' to identify significant words and sentences, perform sentiment analysis, and uncover latent topics through topic modeling. Key features included corpus preprocessing, sentiment analysis, topic modeling (LDA, CTM), and visualizations such as word clouds and sentiment trajectories."
   },
   {
-    id: 6,
+    id: 7,
     title: "Covid Webiste Using Full Stack Web",
     projectLink:"https://github.com/urstrulyrithik/rithik-covidWebsite",
     img: "https://images.pexels.com/photos/3992933/pexels-photo-3992933.jpeg?auto=compress&cs=tinysrgb&w=1600",
     description: "Designed and developed a Covid Website Using Full Stack Development from end to end using React JS, SQL and Express JS and CSS, whose objective is to display statistics and content regarding the pandemic and the Corona virus to users."
   },
   {
-    id: 7,
+    id: 8,
     title: "Jobby App",
     projectLink:"https://rithikjobbyapp.ccbp.tech/login",
     img: "https://images.pexels.com/photos/392018/pexels-photo-392018.jpeg?auto=compress&cs=tinysrgb&w=1600",
     description: "Designed and developed Jobby App, a front-end ReactJS website for PC users, whose objective is to enable users to discover the jobs they seek. CREDENTIALS--> USERNAME: rahul ; PASSWORD: rahul@2021"
   },
   {
-    id: 8,
+    id: 9,
     title: "Twitter Backend Clone",
     projectLink:"https://github.com/urstrulyrithik/TwitterClone",
     img: "https://images.pexels.com/photos/5417837/pexels-photo-5417837.jpeg?auto=compress&cs=tinysrgb&w=1600",
     description: "Designed Backend Twitter Clone REST APIs to recreate and carry out Twitter's backend database CRUD operations using JSON format, MySQL and Node JS (Express JS)"
   },
   {
-    id: 9,
+    id: 10,
     title: "My Portfolio Site",
     img: "./PortfolioHero.png",
     projectLink:"https://github.com/urstrulyrithik/Rithik-Portfolio",
