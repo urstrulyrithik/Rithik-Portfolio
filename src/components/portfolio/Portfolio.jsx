a39078db378ccd1d11f186934e2da19faa0ea1b8
Vianny Rithik Reddy P <99172274+urstrulyrithik@users.noreply.github.com>
2025-02-02 12:22:18 -0500

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -25,50 +25,57 @@ const items = [
     img: "./OperationsAnalyst.jpg",
     description: "I automated ETL processes to centralize over 1M data rows in Snowflake, reducing manual data entry by 30%. Using SAS and SQL, I cleaned and transformed data to develop Linear Regression and ARIMA models with 85% forecast accuracy, lowering MAPE by 12%. My optimization of inventory management helped reduce excess stock and prevent stockouts, saving $96K annually and safeguarding $120K in revenue. Additionally, I developed comprehensive Tableau dashboards for over 50 retail locations using AWS EC2 and Amazon S3, which enhanced data-driven decisions and improved operational performance by 40%."
   },
-  {
+    {
     id: 4,
+    title: "Software Developer Intern",
+    projectLink:"https://github.com/urstrulyrithik",
+    img: "./bbSDE.jpg",
+    description: "During my internships at BigBasket, I contributed to high-impact projects that significantly enhanced both user experience and backend efficiency. I optimized the main website’s shopping cart and checkout modules using React, Node.js/Express, and SQL—achieving a 30% reduction in page load times and a 15% boost in completed transactions. Additionally, I developed a next-generation inventory management system with React, Node.js, Express, and MongoDB, which improved real-time stock tracking and reduced inventory discrepancies by 30% across 30+ retail locations. By collaborating with cross-functional teams and leveraging Agile methodologies, I implemented robust CI/CD pipelines and automated testing frameworks that ensured system reliability and scalability, even during high-traffic periods."
+  },
+  {
+    id: 5,
     title: "Expense Tracking System",
     projectLink:"https://github.com/urstrulyrithik/Project_Expense_Tracking_System",
     img: "./ExpenseTracking.png",
     description: "An Expense Management System featuring a Streamlit-based frontend and a FastAPI backend, designed for efficient tracking and management of personal expenses. The project includes a modular structure with dedicated components for frontend, backend, and comprehensive testing."
   },
   {
-    id: 5,
+    id: 6,
     title: "Predictive Modeling",
     projectLink:"https://github.com/urstrulyrithik/Predictive-Modeling",
     img: "./PredictiveAnalytics.jpg",
     description: "This project develops a predictive model in R to analyze and forecast based on historical data, leveraging machine learning techniques and statistical analysis. Key features include data preprocessing, exploratory data analysis, model training and selection, performance evaluation, and result visualization."
   },
   {
-    id: 6,
+    id: 7,
     title: "Text Analytics",
     projectLink:"https://github.com/urstrulyrithik/Text-Analytics",
     img: "./TextAnalytics.jpeg",
     description: "This project involved extensive text mining and social network analysis in R, focusing on the book 'Tarzan of the Apes' to identify significant words and sentences, perform sentiment analysis, and uncover latent topics through topic modeling. Key features included corpus preprocessing, sentiment analysis, topic modeling (LDA, CTM), and visualizations such as word clouds and sentiment trajectories."
   },
   {
-    id: 7,
+    id: 8,
     title: "Covid Webiste Using Full Stack Web",
     projectLink:"https://github.com/urstrulyrithik/rithik-covidWebsite",
     img: "https://images.pexels.com/photos/3992933/pexels-photo-3992933.jpeg?auto=compress&cs=tinysrgb&w=1600",
     description: "Designed and developed a Covid Website Using Full Stack Development from end to end using React JS, SQL and Express JS and CSS, whose objective is to display statistics and content regarding the pandemic and the Corona virus to users."
   },
   {
-    id: 8,
+    id: 9,
     title: "Jobby App",
     projectLink:"https://rithikjobbyapp.ccbp.tech/login",
     img: "https://images.pexels.com/photos/392018/pexels-photo-392018.jpeg?auto=compress&cs=tinysrgb&w=1600",
     description: "Designed and developed Jobby App, a front-end ReactJS website for PC users, whose objective is to enable users to discover the jobs they seek. CREDENTIALS--> USERNAME: rahul ; PASSWORD: rahul@2021"
   },
   {
-    id: 9,
+    id: 10,
     title: "Twitter Backend Clone",
     projectLink:"https://github.com/urstrulyrithik/TwitterClone",
     img: "https://images.pexels.com/photos/5417837/pexels-photo-5417837.jpeg?auto=compress&cs=tinysrgb&w=1600",
     description: "Designed Backend Twitter Clone REST APIs to recreate and carry out Twitter's backend database CRUD operations using JSON format, MySQL and Node JS (Express JS)"
   },
   {
-    id: 10,
+    id: 11,
     title: "My Portfolio Site",
     img: "./PortfolioHero.png",
     projectLink:"https://github.com/urstrulyrithik/Rithik-Portfolio",
